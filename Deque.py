0752141d021666c5e4d7a235ce1714be986aa7bf
Joshua Swick <joshuaswickirl@gmail.com>
2019-02-15 20:00:09 -0700

    Stubbed Deque class and required methods

---
+++ Deque.py
@@ -0,0 +1,22 @@
+class Deque():
+
+    def __init__(self):
+        self.deque = []
+
+    def addFront(self,item):
+        pass
+
+    def addRear(self,item):
+        pass
+
+    def removeFront(self):
+        pass
+
+    def removeRear(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def size(self):
+        pass
