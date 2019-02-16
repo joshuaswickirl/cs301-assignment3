d1fb370dfb806256fcc8ac0151c5adc493ef5b2f
Joshua Swick <joshuaswickirl@gmail.com>
2019-02-15 20:24:49 -0700

    Stubbed rpn function

---
+++ Reverse_Polish_Notation.py
@@ -0,0 +1,17 @@
+#
+# Reverse Polish 
+#
+# https://en.wikipedia.org/wiki/Reverse_Polish_notation
+#
+
+def rpn(input_string):
+    """
+    Returns evaluated reverse polish notation expression
+
+    Input is a string, containing an expression in 
+    reverse polish notation. Expression is evaluated and 
+    returns the output as a string.
+    
+    ex. rpn("3 4 +") = "7" 
+    """
+    pass
