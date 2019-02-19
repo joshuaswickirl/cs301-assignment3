f357b8c8a408cbe13f27112cbf3ddc95558cd1f6
laurensimms <47647065+laurensimms@users.noreply.github.com>
2019-02-19 15:54:49 -0700

    Update Deque.py

--- Deque.py
+++ Deque.py
@@ -1,22 +1,45 @@
 class Deque():
 
     def __init__(self):
+        """
+        Constructor method for class Deque
+        """
         self.deque = []
+       
 
     def addFront(self,item):
+        """
+        Adds a new item to the front of the deque. Requires item and returns nothing 
+        """
         pass
 
     def addRear(self,item):
+        """
+        Adds a new item to the rear of the deque. Requires item and returns nothing
+        """
         pass
 
     def removeFront(self):
+        """
+        Removes the front item from deque. Requires no parameters and returns
+        item. deque is modified.
+        """
         pass
 
     def removeRear(self):
+        """
+        Removes the item from the rear of the deque. Requires no parameters and returns the item. Dque is modified.
+        """
         pass
 
     def isEmpty(self):
+        """
+        Tests to see if the deque is empty. Requires no parameters and returns boolean value. 
+        """
         pass
 
     def size(self):
+        """
+        Returns the count of items in deque. Requires no parameters and returns int
+        """
         pass
