2fe7aa60f2e687e8cd34c5e12c28276ba297588d
laurensimms <47647065+laurensimms@users.noreply.github.com>
2019-02-23 19:10:08 -0700

    Update Deque.py
    added running time for methods

--- Deque.py
+++ Deque.py
@@ -3,6 +3,7 @@ class Deque():
     def __init__(self):
         """
         Constructor method for class Deque.
+        Runs in constant time 
         """
         self.deque = []
        
@@ -15,6 +16,7 @@ class Deque():
     def addRear(self,item):
         """
         Adds a new item to the rear of the deque. Requires item and returns nothing.
+        Runs in constant time
         """
         self.deque.append(item)
 
@@ -22,6 +24,7 @@ class Deque():
         """
         Removes the front item from deque. Requires no parameters and returns
         item. Deque is modified.
+        Runs in constant time
         """
         item = self.deque[0]
         self.deque.remove(self.deque[0])
@@ -29,7 +32,9 @@ class Deque():
 
     def removeRear(self):
         """
-        Removes the item from the rear of the deque. Requires no parameters and returns the item. Dque is modified.
+        Removes the item from the rear of the deque. 
+        Requires no parameters and returns the item. Deque is modified.
+        Runs in constant time
         """
         item = self.deque[-1]
         self.deque.remove(self.deque[-1])
@@ -38,6 +43,7 @@ class Deque():
     def isEmpty(self):
         """
         Tests to see if the deque is empty. Requires no parameters and returns boolean value. 
+        Runs in constant time
         """
         if len(self.deque) == 0:
             return True
@@ -47,5 +53,6 @@ class Deque():
     def size(self):
         """
         Returns the count of items in deque. Requires no parameters and returns int.
+        Runs in constant time
         """
         return len(self.deque)
