21d67eb34e00286389f05f019c3c661a50e4d8a0
Joshua Swick <joshuaswickirl@gmail.com>
2019-02-24 12:07:57 -0700

    Reformat docstrings

--- Deque.py
+++ Deque.py
@@ -3,20 +3,24 @@ class Deque():
     def __init__(self):
         """
         Constructor method for class Deque.
-        Runs in constant time 
+        
+        Runs in constant time.
         """
         self.deque = []
        
     def addFront(self,item):
         """
         Adds a new item to the front of the deque. Requires item and returns nothing.
+        
+        Runs in constant time.
         """
         self.deque.insert(0,item)
 
     def addRear(self,item):
         """
         Adds a new item to the rear of the deque. Requires item and returns nothing.
-        Runs in constant time
+        
+        Runs in constant time.
         """
         self.deque.append(item)
 
@@ -24,7 +28,8 @@ class Deque():
         """
         Removes the front item from deque. Requires no parameters and returns
         item. Deque is modified.
-        Runs in constant time
+        
+        Runs in constant time.
         """
         item = self.deque[0]
         self.deque.remove(self.deque[0])
@@ -34,7 +39,8 @@ class Deque():
         """
         Removes the item from the rear of the deque. 
         Requires no parameters and returns the item. Deque is modified.
-        Runs in constant time
+        
+        Runs in constant time.
         """
         item = self.deque[-1]
         self.deque.remove(self.deque[-1])
@@ -43,7 +49,8 @@ class Deque():
     def isEmpty(self):
         """
         Tests to see if the deque is empty. Requires no parameters and returns boolean value. 
-        Runs in constant time
+        
+        Runs in constant time.
         """
         if len(self.deque) == 0:
             return True
@@ -53,6 +60,7 @@ class Deque():
     def size(self):
         """
         Returns the count of items in deque. Requires no parameters and returns int.
-        Runs in constant time
+        
+        Runs in constant time.
         """
         return len(self.deque)
